e383a3aff0133e9d4adfe329e87175eacbd282cc
RonZatkovsky <167799853+RonZatkovsky@users.noreply.github.com>
2024-10-25 22:08:01 +0000

    added library to make a picture out of a react component. there are problems with the positioning

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,34 @@
-import React, { useContext } from "react";
-import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
+import { exportComponentAsJPEG, exportComponentAsPDF, exportComponentAsPNG } from 'react-component-export-image';
+import React, { useRef } from 'react';
 
-export const Home = () => {
-	const { store, actions } = useContext(Context);
+const ComponentToPrint = React.forwardRef((props, ref) => (
+  <div ref={ref} className='position-relative' style={{height:'100'}}>
+	<img className='position-absolute opacity-50' style={{zIndex:-1}} src='https://www.pbs.org/wnet/nature/files/2024/01/GoldenHeadedLionTamarin.jpg'></img>
+	<img className='position-absolute' style={{zIndex:0}} src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Bonnet_macaque_%28Macaca_radiata%29_Photograph_By_Shantanu_Kuveskar.jpg/220px-Bonnet_macaque_%28Macaca_radiata%29_Photograph_By_Shantanu_Kuveskar.jpg'></img>
+  </div>
+));
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
+export const MyComponent = () => {
+  const componentRef = useRef();
+
+  return (
+    <div>
+		<div className='position-relative'>
+			<React.Fragment >
+				<ComponentToPrint ref={componentRef} />
+			</React.Fragment>
 		</div>
-	);
+		<div className=' top-0'>
+			<button onClick={() => exportComponentAsJPEG(componentRef)}>
+				Export As JPEG
+			</button>
+			<button onClick={() => exportComponentAsPDF(componentRef)}>
+				Export As PDF
+			</button>
+			<button onClick={() => exportComponentAsPNG(componentRef)}>
+				Export As PNG
+			</button>
+		</div>
+	</div>
+  );
 };
